9bb600de32f9986fdef219c2034d508f3c15f8ce
Vitaly Pavlenko <vitaly-pavlenko@192.168.1.224>
2025-05-11 21:49:33 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState} from 'react'
+import './App.css'
+
+function App() {
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+  const handleClick = async () => {
+          const res = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+              method: 'POST',
+              headers: {
+                  Authorization: 'Bearer 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',
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                  model: 'deepseek/deepseek-prover-v2:free',
+                  messages: [
+                      {
+                          role: 'user',
+                          content: value,
+                      },
+                  ],
+                  "max_tokens": 1000
+              }),
+          });
+
+          const finalRes = await res.json();
+          setResult(finalRes.choices?.[0]?.message?.content);
+  }
+
+  return (
+    <>
+        <p><input type="text" value={value} onChange={(e) => setValue(e.target.value)}/></p>
+        <p>{result}</p>
+        <p><button onClick={handleClick}>
+            Send
+        </button></p>
+    </>
+  )
+}
+
+export default App
