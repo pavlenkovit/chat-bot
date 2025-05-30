b33076e69b7b1611c65116427d093d364bed4422
Vitaly Pavlenko <vitpvln@gmail.com>
2025-05-30 17:00:07 +0200

    upd

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/chat-bot">
       <App />
     </BrowserRouter>
   </React.StrictMode>
